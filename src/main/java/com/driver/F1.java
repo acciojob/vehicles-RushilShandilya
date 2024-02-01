b18427a9be9c500d04eba232513ece50dd22d860
RushilShandilya <rushilcujoh@gmail.com>
2024-02-01 23:30:52 +0530

    Commit

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -24,6 +24,13 @@ public class F1 extends Car {
             stop();
             setGear(1);
         }
+        if(newSpeed>=1 && newSpeed<=50) setGear(1);
+        else if(newSpeed>=51 && newSpeed<=100) setGear(2);
+        else if(newSpeed>=101 && newSpeed<=150) setGear(3);
+        else if(newSpeed>=151 && newSpeed<=200) setGear(4);
+        else if(newSpeed>=201 && newSpeed<=250) setGear(5);
+        else setGear(6);
+
         //for all other cases, change the gear accordingly
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
