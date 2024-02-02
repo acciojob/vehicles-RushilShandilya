94f01f52003133a2154af2a077ae94981a49aec3
RushilShandilya <rushilcujoh@gmail.com>
2024-02-02 18:28:13 +0530

    Commit

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -27,7 +27,8 @@ public class Vehicle {
 
     public void steer(int direction){
         //direction is in degrees, add it to the current direction
-        int newDirection = getCurrentDirection() + direction;
+        int newDirection = getCurrentDirection()+direction;
+        if(newDirection>360) newDirection -=360;
         setCurrentDirection(newDirection);
         System.out.println("steer method called - The direction is changed to: " + getCurrentDirection() + " degrees");
     }
